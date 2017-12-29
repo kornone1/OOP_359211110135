387c0d5ac0dbd67a4256eac689a8b41df528c390
LAB_COM-MT_1 <kornone2535@gmail.com>
2017-12-29 09:48:36 +0700

    TestInput

--- src/ooplab6/myStrBuff.java
+++ src/ooplab6/myStrBuff.java
@@ -2,6 +2,16 @@ package ooplab6;
 
 public class myStrBuff {
     public static void main(String[] args) {
+    //stringBuffer
+        StringBuffer myBuf = new StringBuffer();
+        myBuf.append("Hello");
+        myBuf.append("Welcome");
+        myBuf.append("RMUTSV");
+        myBuf.append(2017);
+        System.out.println(myBuf);
+
+
+
 
 
     }//main
